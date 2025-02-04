7e929dc025730a71fa5f777c83d7aa4a3cb5feb0
Iamalsauser <parthsinhv4@gmail.com>
2025-02-04 21:59:05 +0530

    Update: try await

--- SupaFeatures/Feature.swift
+++ SupaFeatures/Feature.swift
@@ -1,18 +1,35 @@
 
 
+//import Foundation
+//
+//struct Feature: Codable, Identifiable, Hashable {
+//    var id: Int?
+//    var createdAt: Date
+//    var text: String
+//    var isComplete: Bool
+//    var userID: UUID
+//    
+//    enum CodingKeys: String, CodingKey {
+//        case id, text
+//        case createdAt = "created_at"
+//        case isComplete = "is_complete"
+//        case userID = "user_id"
+//    }
+//}
+
 import Foundation
 
-struct Feature: Codable, Identifiable, Hashable {
-    var id: Int?
-    var createdAt: Date
-    var text: String
-    var isComplete: Bool
-    var userID: UUID
+struct Feature: Codable,Identifiable,Hashable{
+    var id:Int?
+    var createdAt:Date
+    var text:String
+    var isComplete:Bool
+    var userID:UUID
     
-    enum CodingKeys: String, CodingKey {
-        case id, text
-        case createdAt = "created_at"
-        case isComplete = "is_complete"
-        case userID = "user_id"
+    enum CodingKeys: String,CodingKey{
+        case id,text
+        case createdAt="created_at"
+        case isComplete="is_complete"
+        case userID="user_id"
     }
 }
--- SupaFeatures/Secrets.swift
+++ SupaFeatures/Secrets.swift
@@ -1,9 +1,6 @@
-
-
 import Foundation
 
-enum Secrets {
-    static let projectURL = URL(string: "")!
-        
-    static let apiKey = ""
+struct Secrets{
+    static let projectURL=URL(string: "https://yzytlqaaujkwzcgscvkj.supabase.co")!
+    static let apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6eXRscWFhdWprd3pjZ3NjdmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MjY1NzgsImV4cCI6MjA1NDIwMjU3OH0.uNnOqjg83HWC-0bBMxuj2NAvam6Jx4oJncssYUI8AAw"
 }
